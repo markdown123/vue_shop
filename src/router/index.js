6b73f80ef1587401c59057a89782437486bb3570
huangwf <keyperson999@gmail.com>
2020-07-24 11:10:38 +0800

    数据报表功能

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import addGoods from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 
 
@@ -33,6 +34,7 @@ const router = new VueRouter({
       {path: '/goods',component: List},
       {path: '/goods/add',component: addGoods},
       {path: '/orders',component: Order},
+      {path: '/reports',component: Report},
       ]
     },
 
